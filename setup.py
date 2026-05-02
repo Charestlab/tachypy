5c3a0a75c2b122875f213159d0efd91d8fad7892
iancharest <charest.ian@gmail.com>
2026-05-02 14:43:20 -0400

    Bump package version to 0.1.9 for PyPI release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as reqfile:
 
 setup(
     name='tachypy',
-    version='0.1.8',
+    version='0.1.9',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=requires,
